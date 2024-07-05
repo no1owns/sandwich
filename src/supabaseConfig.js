f1fcba1a97a16bfeee2c8d87b1b6b46ebf984677
No1 <ayoakintilo@gmail.com>
2024-07-05 03:34:18 +0000

    Changes to be committed: 	modified:   src/supabaseConfig.js

--- src/supabaseConfig.js
+++ src/supabaseConfig.js
@@ -1,7 +1,7 @@
 // src/supabaseConfig.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project.supabase.co';
-const supabaseKey = 'your-anon-key';
+const supabaseUrl = 'https://uzjaizmdgwfyiixwspdp.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6amFpem1kZ3dmeWlpeHdzcGRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk0NTAyNTUsImV4cCI6MjAzNTAyNjI1NX0.7HEUiRGbVWTv8St_PEcwiTVjazkCRDwK324oPJuF7Ns';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
