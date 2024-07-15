40d98562c292963133a3a1550ceb5bc0cbd69082
No1 <ayoakintilo@gmail.com>
2024-07-15 03:34:34 +0000

    modified:   public/src/photoUpload.js

--- public/src/photoUpload.js
+++ public/src/photoUpload.js
@@ -1,5 +1,10 @@
 // src/photoUpload.js
-import { supabase } from './supabaseConfig.js';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+const supabaseUrl = 'https://uzjaizmdgwfyiixwspdp.supabase.co'; // Replace with your Supabase URL
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6amFpem1kZ3dmeWlpeHdzcGRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk0NTAyNTUsImV4cCI6MjAzNTAyNjI1NX0.7HEUiRGbVWTv8St_PEcwiTVjazkCRDwK324oPJuF7Ns';
+
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 document.getElementById('photo-upload-form').addEventListener('submit', async (e) => {
   e.preventDefault();
